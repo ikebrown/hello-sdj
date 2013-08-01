866c88b3d51724f5e1a8177ce56d66565dc3e8bb
Michael D'Agosta <mdagosta@codebug.com>
2013-08-01 15:05:59 -0700

    Hello Form implementation. Add thanks page and we're almost done.

---
+++ django_sdj/hello/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from hello import views
+
+urlpatterns = patterns('',
+    url(r'^hello$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^thanks$', views.thanks, name='thanks'),
+)
--- django_sdj/hello/views.py
+++ django_sdj/hello/views.py
@@ -1,7 +1,28 @@
-from django.http import HttpResponse
+import logging as log
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
+from django import forms
+from django.shortcuts import render
+
+
+class HelloForm(forms.Form):
+    email = forms.EmailField(required=True, label='Your Email Address')
+    message = forms.CharField(max_length=4000, required=True, label='Your Message to me', widget=forms.Textarea)
+
 
 def index(request):
     template = loader.get_template('hello/index.html')
-    context = RequestContext(request, {'hello_who': 'SDJournal.org'})
+    tmpl = {'hello_who': 'SDJournal.org'}
+    if request.method == 'POST':
+        form = HelloForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks')
+    else:
+        form = HelloForm()
+    tmpl['form'] = form
+    context = RequestContext(request, tmpl)
     return HttpResponse(template.render(context))
+
+
+def thanks(request):
+    return render(request, 'hello/thanks.html')
